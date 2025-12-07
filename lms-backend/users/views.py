a3c06568120f82ac32c33a933a1cf6d052ffeb30
GirishChaulagain <girishchaulagain5@gmail.com>
2025-12-07 18:46:29 +0545

    feat(users): implement viewsets for user related functionalities

--- lms-backend/users/views.py
+++ lms-backend/users/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import User, ILSResponse, LearningStyleScore, UserLog
+from .serializers import (
+    UserSerializer,
+    ILSResponseSerializer,
+    LearningStyleScoreSerializer,
+    UserLogSerializer
+)
+
 
 # Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class ILSResponseViewSet(viewsets.ModelViewSet):
+    queryset = ILSResponse.objects.all()
+    serializer_class = ILSResponseSerializer
+
+
+class LearningStyleScoreViewSet(viewsets.ModelViewSet):
+    queryset = LearningStyleScore.objects.all()
+    serializer_class = LearningStyleScoreSerializer
+
+
+class UserLogViewSet(viewsets.ModelViewSet):
+    queryset = UserLog.objects.all()
+    serializer_class = UserLogSerializer
