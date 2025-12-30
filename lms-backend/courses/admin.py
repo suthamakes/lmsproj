793593e6624d0c9fbdfcaea8dee8dd53ccce04fb
GirishChaulagain <girishchaulagain5@gmail.com>
2025-12-30 21:10:51 +0545

    chore: add initial admin setup for courses app

--- lms-backend/courses/admin.py
+++ lms-backend/courses/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Course, Module, ContentItem, QuizQuestion
 
 # Register your models here.
+
+admin.site.register(Course)
+admin.site.register(Module)
+admin.site.register(ContentItem)
+admin.site.register(QuizQuestion)
